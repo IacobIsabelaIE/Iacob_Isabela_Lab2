80211f58997c7a92725cda5271370738d83f19bb
IacobIsabelaIE <isabela.iacob@stud.ubbcluj.ro>
2023-11-05 23:29:57 +0200

    modificari lab4

--- Pages/Publishers/Index.cshtml.cs
+++ Pages/Publishers/Index.cshtml.cs
@@ -7,26 +7,39 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using Iacob_Isabela_Lab2.Data;
 using Iacob_Isabela_Lab2.Models;
+using Iacob_Isabela_Lab2.Models.ViewModels;
 
 namespace Iacob_Isabela_Lab2.Pages.Publishers
 {
     public class IndexModel : PageModel
     {
         private readonly Iacob_Isabela_Lab2.Data.Iacob_Isabela_Lab2Context _context;
-
         public IndexModel(Iacob_Isabela_Lab2.Data.Iacob_Isabela_Lab2Context context)
         {
             _context = context;
         }
-
-        public IList<Publisher> Publisher { get;set; } = default!;
-
-        public async Task OnGetAsync()
+        public IList<Publisher> Publisher { get; set; } = default!;
+        public PublisherIndexData PublisherData { get; set; }
+        public int PublisherID { get; set; }
+        public int BookID { get; set; }
+        public async Task OnGetAsync(int? id, int? bookID)
         {
-            if (_context.Publisher != null)
+
+            PublisherData = new PublisherIndexData();
+            PublisherData.Publishers = await _context.Publisher
+            .Include(i => i.Books)
+            .ThenInclude(c => c.Author)
+            .OrderBy(i => i.PublisherName)
+            .ToListAsync();
+            if (id != null)
             {
-                Publisher = await _context.Publisher.ToListAsync();
+                PublisherID = id.Value;
+                Publisher publisher = PublisherData.Publishers
+                .Where(i => i.ID == id.Value).Single();
+                PublisherData.Books = publisher.Books;
             }
         }
     }
 }
+
+        
